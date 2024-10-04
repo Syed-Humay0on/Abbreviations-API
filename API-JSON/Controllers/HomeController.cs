ef7d17f86bfc06912ff932dbe642d23a3d5feac0
Proto <humayoon04syed@gmail.com>
2024-10-05 01:08:23 +0500

    Add project files.

---
+++ API-JSON/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+using System.Diagnostics;
+using System.Net;
+using System.Text.Json.Nodes;
+using API_JSON.Models;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.WebRequestMethods;
+
+namespace API_JSON.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+//        public IActionResult Abbreviations()
+//        {
+//            return View();
+//;        }
+        [HttpPost]
+        public async Task<IActionResult> Abbreviations(string Search_Abbr)
+		{
+            string URL = "https://www.stands4.com/services/v2/abbr.php?uid=12802&tokenid=H78gmEjXBfNk8WmY&term=" + Search_Abbr +"&format=json";
+			using (HttpClient client = new HttpClient())
+			{
+				try
+				{
+					//WebClient web = new WebClient();
+					string JsonData = await client.GetStringAsync(URL);
+					JsonNode node = JsonNode.Parse(JsonData);
+					ViewBag.json = JsonData;
+
+					
+
+					if (node != null)
+					{
+
+					}
+					return View();
+				}
+				catch (HttpRequestException ex)
+				{
+					// Handle the exception and log it if necessary
+					ViewBag.Error = "Error fetching data from the API: " + ex.Message;
+					return View("Error"); // Display an error view
+				}
+			}
+		}
+        
+    }
+}
---
+++ API-JSON/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace API_JSON.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
